d33dcf863763e887e54e53a7c726eb02a7c4c341
Dani2202 <daniyalq2202@gmail.com>
2025-08-28 02:09:06 +0500

    minimal deploy

--- app/layout.js
+++ app/layout.js
@@ -28,24 +28,16 @@ const robotoMono = Roboto_Mono({
 });
 
 export const metadata = {
-  title: "Portfolio of Daniyal Qureshi - Software Engineer",
-  description:
-    "This is the portfolio of Daniyal Qureshi, a Software Engineer with expertise in React.js, Next.js, TypeScript, and modern web development. I have experience working on complex projects including shareholder voting platforms, AI-driven dashboards, and comprehensive management systems. I am passionate about building scalable applications and implementing best practices like ABAC, RBAC, and custom component development.",
+  title: "Test",
+  description: "Test",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${poppins.variable} ${robotoMono.variable}`}>
-        {/* <ToastContainer /> */}
-        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          <Navbar />
-          {children}
-          {/* <ScrollToTop /> */}
-        </main>
-        <Footer />
+      <body>
+        {children}
       </body>
-      {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} /> */}
     </html>
   );
 }
