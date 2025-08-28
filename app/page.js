afbed045d8ce2b8150ef5fdf0bfeacf9d1c14e02
Dani2202 <daniyalq2202@gmail.com>
2025-08-28 11:08:46 +0500

    Experience Added

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
 // import ContactSection from "./components/homepage/contact";
 // import Education from "./components/homepage/education";
-// import Experience from "./components/homepage/experience";
+import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 // import Projects from "./components/homepage/projects";
 // import Skills from "./components/homepage/skills";
@@ -15,8 +15,8 @@ export default function Home() {
     <div>
       <HeroSection />
       <AboutSection />
-      {/* <Experience />
-      <Skills />
+      <Experience />
+      {/* <Skills />
       <Projects />
       <Education />
       <ContactSection /> */}
