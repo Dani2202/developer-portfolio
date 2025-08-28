fd4281679ea24a2b98e91ad889c109c0fad32db6
Dani2202 <daniyalq2202@gmail.com>
2025-08-28 11:06:44 +0500

    About Section Added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { personalData } from "@/utils/data/personal-data";
-// import AboutSection from "./components/homepage/about";
+import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
 // import ContactSection from "./components/homepage/contact";
 // import Education from "./components/homepage/education";
@@ -14,8 +14,8 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
-      {/* <AboutSection />
-      <Experience />
+      <AboutSection />
+      {/* <Experience />
       <Skills />
       <Projects />
       <Education />
