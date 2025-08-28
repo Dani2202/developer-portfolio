9f527f96ca8417c8e08ad9aad4dd3e4e2653c1a4
Dani2202 <daniyalq2202@gmail.com>
2025-08-28 11:13:00 +0500

    Skills Added

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import AboutSection from "./components/homepage/about";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 // import Projects from "./components/homepage/projects";
-// import Skills from "./components/homepage/skills";
+import Skills from "./components/homepage/skills";
 
 export default function Home() {
   return (
@@ -16,8 +16,8 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <Experience />
-      {/* <Skills />
-      <Projects />
+      <Skills />
+      {/* <Projects />
       <Education />
       <ContactSection /> */}
     </div>
