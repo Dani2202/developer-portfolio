8b56b03d9fb702de366dade38f2e5b4000c50119
Dani2202 <daniyalq2202@gmail.com>
2025-08-28 11:00:49 +0500

    Minimal Deployment

--- app/page.js
+++ app/page.js
@@ -1,26 +1,27 @@
 'use client';
 
 import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
+// import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+// import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+// import HeroSection from "./components/homepage/hero-section";
+// import Projects from "./components/homepage/projects";
+// import Skills from "./components/homepage/skills";
 
 export default function Home() {
   return (
     <div>
-      <HeroSection />
+      <h1>Test Page</h1>
+      <p>Testing to isolate SSR issue</p>
+      {/* <HeroSection />
       <AboutSection />
       <Experience />
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
-      <ContactSection />
+      <ContactSection /> */}
     </div>
   )
 };
